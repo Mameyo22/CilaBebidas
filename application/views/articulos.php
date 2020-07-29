0d09c3d87323a1023ef7134db949804ed25e7b7c
Marcelo Diaz <marcelodiaz22@gmail.com>
2020-07-28 21:37:17 -0300

    eliminar

--- application/views/articulos.php
+++ application/views/articulos.php
@@ -24,8 +24,9 @@
                     <td><img class="img-product" src="<?=base_url().'/img/products/'.$articulo['articuloimg']; ?>" alt="Sin Imagen">  </td>
                     <td>
                         <a href="<?= base_url(); ?>index.php/cila/editarticulo/<?= $articulo['articuloid']; ?> ">
-                            <button type="button" class="btn btn-success" data-toggle="tooltip" title="Editar"><i class="fa fa-edit"></i></button>
+                            <button type="button" class="btn btn-success" title="Editar"><i class="fa fa-edit"></i></button>
                         </a>
+                        <button type="button" class="btn btn-danger btn_dlt" title="Eliminar" data-id="<?= $articulo['articuloid'];?>"  data-toggle="modal" data-target="#myModal"><i class="fa fa-trash-o"></i></button>
                     </td>
                 </tr>
                 <?php } ?>
@@ -34,9 +35,42 @@
      </section>
     </section>
     <!--main content end-->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Eliminar Artículo</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                    Esta seguro de eliminar este artículo?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-danger" id="eliminar">Eliminar</button>
+                </div>
+            </div>
+        </div>
+    </div>        
+
 <script>
 $(document).ready(function(){
+    var id = 0;
     $('#arttable').DataTable();
+
+     $('.btn_dlt').click(function(){
+        //obtener el id
+        id = $(this).attr('data-id');
+    });
+    //Eliminar articulo
+    $("#eliminar").click(function(){
+
+        //usar ajax para eliminar el registro
+
+        //Ocultar modal
+        $("#myModal").modal('hide');
+       
+    });
 });
 </script>
  
